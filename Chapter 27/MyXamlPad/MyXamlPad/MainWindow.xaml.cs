1c2491d0fefa9f89edc55942a96f271783e7f8e2
delve <delve202@gmail.com>
2014-09-06 08:53:26 -0400

    finished ch 27

---
+++ Chapter 27/MyXamlPad/MyXamlPad/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿namespace MyXamlPad
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Markup;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // when the main window loads we'll load the xaml from the last execution or some
+            // basic starter xaml
+            if (File.Exists("YourXaml.xaml"))
+            {
+                txtXamlData.Text = File.ReadAllText("YourXaml.xaml");
+            }
+            else
+            {
+                txtXamlData.Text =
+                    "<Window xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"\n" +
+                    "    xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\"\n" +
+                    "    Height=\"400\" Width=\"500\" WindowStartupLocation=\"CenterScreen\">\n" +
+                    "  <StackPanel>\n  </StackPanel>\n" +
+                    "</Window>";
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            // save the input
+            File.WriteAllText("YourXaml.xaml", txtXamlData.Text);
+            Application.Current.Shutdown();
+        }
+
+        private void BtnViewXaml_Click(object sender, RoutedEventArgs e)
+        {
+            // serialize the user input
+            File.WriteAllText("YourXaml.xaml", txtXamlData.Text);
+
+            // target window for the serializewd XAML
+            Window myWindow = null;
+            try
+            {
+                using (Stream sr = File.Open("YourXaml.xaml", FileMode.Open))
+                {
+                    // connect the XAML to the window
+                    myWindow = (Window)XamlReader.Load(sr);
+
+                    // show as a modal dialog
+                    myWindow.ShowDialog();
+                    myWindow.Close();
+                    myWindow = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Chapter 27/WpfAppAllCode/WpfAppAllCode/MainWindow.cs
@@ -0,0 +1,88 @@
+﻿namespace WpfAppAllCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class MainWindow : Window
+    {
+        private Button exitAppBtn = new Button();
+        private string baseButtonText = "Exit Application";
+        private string baseTitle;
+
+        public MainWindow(string windowTitle, int height, int width)
+        {
+            // Configure the button and set the child control
+            this.exitAppBtn.Click += new RoutedEventHandler(this.ExitAppBtn_Clicked);
+            this.exitAppBtn.Content = this.baseButtonText;
+            this.exitAppBtn.Height = 25;
+            this.exitAppBtn.Width = 150;
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.Show();
+
+            // set window content to this single button
+            this.Content = this.exitAppBtn;
+            
+            // configure the window
+            this.baseTitle = windowTitle;
+            this.Title = this.baseTitle;
+            this.Height = height;
+            this.Width = width;
+
+            // prevent unintended closing
+            this.Closing += this.MainWindow_Closing;
+            this.Closed += this.MainWindow_Closed;
+
+            // Mouse input routines
+            this.MouseMove += this.MainWindow_MouseMove;
+
+            // Keyboard input routines
+            this.KeyDown += this.MainWindow_KeyDown;
+        }
+
+        private void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            this.exitAppBtn.Content = string.Format("{0} - Key: {1}", this.baseButtonText, e.Key.ToString());
+        }
+
+        private void MainWindow_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.Title = string.Format("{0} : Your mouse is at {1}", this.baseTitle, e.GetPosition(this).ToString());
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            MessageBox.Show("Buhbye");
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // does the user really want to quit?
+            string msg = "Do you really really really wanna quit?\nTime for quiche?";
+            MessageBoxResult result = MessageBox.Show(msg, "My App", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+            if (MessageBoxResult.No == result)
+            {
+                // cancel crash
+                e.Cancel = true;
+            }
+        }
+
+        // button click handler
+        private void ExitAppBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            // were we cheating? <gasp>
+            if ((bool)Application.Current.Properties["GodMode"])
+            {
+                MessageBox.Show("Cheatin' eh? You must be busy.");
+            }
+
+            // close the window
+            this.Close();
+        }
+    }
+}
