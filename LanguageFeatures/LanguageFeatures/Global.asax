cb7543ba922d72568a8830dcbff6ccd93ad49b37
delve <delve202@gmail.com>
2014-10-05 12:52:46 -0400

    Completed ch 5

--- LanguageFeatures/LanguageFeatures/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LanguageFeatures.MvcApplication" Language="C#" %>
---
+++ ProAspNetMvc5_5thEd/Chapter 05/Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        [HttpGet]
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+
+        [HttpGet]
+        public  ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
+
+        [HttpGet]
+        public ActionResult DemoArray()
+        {
+            Product[] array = {
+                    new Product{
+                        Name="Kayak",
+                        Price=275M},
+                    new Product{
+                        Name="Lifejacket",
+                        Price=48.95M},
+                    new Product{
+                        Name="Soccer ball",
+                        Price=19.5M},
+                    new Product{
+                        Name="Corner flag",
+                        Price=34.95M}
+                };
+            return View(array);
+        }
+    }
+}
